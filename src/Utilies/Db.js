0bfc42bb28732797d8b5b2b4e366337b6cc52553
Fahad <fahadlatif337@gmail.com>
2024-03-15 13:01:27 +0500

    change image

--- src/Utilies/Db.js
+++ src/Utilies/Db.js
@@ -2,7 +2,7 @@
     {
       id: 1,
       name:'Banff national Park',
-      img: require('../Assets/Images/Index/M-1.jpg'),
+      img: require('../Assets/Images/Index/m-i.jpg'),
       cr: require('../Assets/Images/Index/star.webp'),
       titel:"4.9(7K reviews)",
       text: 'Travel',
@@ -12,7 +12,7 @@
       id: 2,
       titel:"6.9(7K reviews)",
       name:'Banff national Park',
-      img: require('../Assets/Images/Index/M-2.jpg'),
+      img: require('../Assets/Images/Index/m-2.jpg'),
       text: 'Urban',
       des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
     },
@@ -20,7 +20,7 @@
       id: 3,
       titel:"2.9(7K reviews)",
       name:'Banff national Park',
-      img: require('../Assets/Images/Index/M-3.jpg'),
+      img: require('../Assets/Images/Index/m-3.jpg'),
       text: 'Opening',
       des: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
@@ -28,7 +28,7 @@
       id: 4,
       name:'Banff national Park',
       titel:"4.0(70K reviews)",
-      img: require('../Assets/Images/Index/M-4.jpg'),
+      img: require('../Assets/Images/Index/m-4.jpg'),
       text: 'Opening',
       des: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
     },
@@ -36,7 +36,7 @@
       id: 5,
       name:'Banff national Park',
       titel:"3.9(2K reviews)",
-      img: require('../Assets/Images/Index/M-5.jpg'),
+      img: require('../Assets/Images/Index/m-5.jpg'),
       text: 'Travel',
       des: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
@@ -44,7 +44,7 @@
       id: 6,
       name:'Banff national Park',
       titel:"6.9(9K reviews)",
-      img: require('../Assets/Images/Index/M-6.jpg'),
+      img: require('../Assets/Images/Index/m-6.jpg'),
       text: 'Travel',
       des: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.ut labore et dolore magna aliqua"
     },
